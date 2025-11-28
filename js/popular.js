8a31e035c0b66606a656a01ccf65043fc8dbbad2
abhishekkumar177 <abhishekumar1772004@gmail.com>
2025-11-28 21:49:14 +0530

    Different Files Created !! Need to rectify

---
+++ js/popular.js
@@ -0,0 +1,9 @@
+// Initialize AOS Animation
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+        duration: 800,
+        easing: 'ease-out',
+        once: true,
+        offset: 100
+    });
+});
